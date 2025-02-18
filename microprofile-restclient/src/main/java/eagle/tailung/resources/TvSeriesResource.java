0783043b77371cc423b7f05784c25f9bb19926fe
shahazada talib <shahazada.talib@navigaglobal.com>
2025-02-18 22:35:30 +0530

    Added fallback method

--- microprofile-restclient/src/main/java/eagle/tailung/resources/TvSeriesResource.java
+++ microprofile-restclient/src/main/java/eagle/tailung/resources/TvSeriesResource.java
@@ -4,6 +4,8 @@ import io.vertx.core.json.JsonArray;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.faulttolerance.Fallback;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import java.util.List;
@@ -16,9 +18,13 @@ public class TvSeriesResource {
     @RestClient
     TvSeriesPersonProxy personProxy;
     @GET
+    @Fallback(fallbackMethod = "getTvSeriesByIdFallback")
     @Path("{id}")
-    public TvSeries getTvSeriesById(@PathParam("id") int id){
-        return proxy.getTvSeriesById(id);
+    public Response getTvSeriesById(@PathParam("id") int id){
+        return Response.ok(proxy.getTvSeriesById(id)).build();
+    }
+    public Response getTvSeriesByIdFallback(int id){
+        return Response.ok("Site is under maintenance").build();
     }
     @GET
     @Path("/person/{personName}")
