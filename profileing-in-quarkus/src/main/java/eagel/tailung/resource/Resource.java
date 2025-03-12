8a52efcfa2309a7ffbe189d0a5ef924d004d9ab7
shahazada talib <shahazada.talib@navigaglobal.com>
2025-03-12 19:32:13 +0530

    multiple profile

--- profileing-in-quarkus/src/main/java/eagel/tailung/resource/Resource.java
+++ profileing-in-quarkus/src/main/java/eagel/tailung/resource/Resource.java
@@ -1,2 +1,18 @@
-package eagel.tailung.resource;public class Resource {
+package eagel.tailung.resource;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@Path("/")
+public class Resource {
+    @ConfigProperty(name = "CEO",defaultValue = "CD")
+    String ceo;
+
+    @GET
+    @Path("ceo")
+    public Response getCEOName(){
+        return Response.ok(ceo).build();
+    }
 }
